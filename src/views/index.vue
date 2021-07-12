c5b3130c6e5c4bfc4a9cc2c345e22752e904901c
swxs_ <466565029@qq.com>
2021-07-12 17:30:57 +0800

    [swxs] feature: 不再使用scoped了

--- src/views/index.vue
+++ src/views/index.vue
@@ -183,7 +183,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="less" scoped>
+<style lang="less">
 .ttype_changer {
   margin-left: 20px;
   cursor: pointer;
